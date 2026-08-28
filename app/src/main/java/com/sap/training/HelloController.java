e4b863669481046a715dffca34b9b90d439032df
ruthika1004 <ruthika.annamnenii@gmail.com>
2026-08-28 14:54:14 +0530

    my pipeline

--- app/src/main/java/com/sap/training/HelloController.java
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Ruthika - DevOps is live!";
+        return "Hello from Ruthika Annamaneni - DevOps is live!";
     }
 }
